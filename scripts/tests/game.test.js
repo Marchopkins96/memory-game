b776028927da1774acc15be0c8802df5dc00be13
Marc Hopkins <marchopkins1@outlook.com>
2024-02-26 16:07:39 +0000

    Update code in newGame function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -5,7 +5,7 @@
 
 const { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn } = require("../game");
 
-
+jest.spyOn(window, "alert").mockImplementation(() => { });
 
 beforeAll(() => {
     let fs = require("fs");
@@ -96,5 +96,10 @@ describe("gameplay works correctly", () => {
         game.playerMoves.push(game.currentGame[0]);
         playerTurn();
         expect(game.score).toBe(1);
+    });
+    test("should call an alert if the move is wrong", () => {
+        game.playerMoves.push("wrong");
+        playerTurn();
+        expect(window.alert).toBeCalledWith("Wrong Move!");
     });
 });
