8bcf875d27560df6593ba06217f4376d5cc0e76c
Marc Hopkins <marchopkins1@outlook.com>
2024-02-26 10:48:46 +0000

    Add/remove code from game.test.js

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,9 +2,9 @@
  * @jest-environment jsdom
  */
 
-const { default: test } = require("node:test");
+
 const { game } = require("../game");
-const { expect } = require("@jest/globals");
+
 
 
 beforeAll(() => {
@@ -27,6 +27,9 @@ describe("game object contains correct keys", () => {
     });
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 
 });
