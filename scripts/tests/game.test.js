ec80541f9b2f759fb77775ce538aeed6d933ede9
Marc Hopkins <marchopkins1@outlook.com>
2024-02-26 11:24:58 +0000

    Add code to game.test.js to replace removed code

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -51,5 +51,8 @@ describe("newGame works correctly", () => {
     test("should clear the player moves array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
-    
+    test("should add one move to the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
+    });
+
 });
